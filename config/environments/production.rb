00a437f931d5e394428d92ff143b6511566bc2b7
Erick Contreras <er.contreras@hotmail.com>
2024-05-23 15:05:49 -0600

    Change name temporal to grades system

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "temporal_production"
+  # config.active_job.queue_name_prefix = "grades_system_production"
 
   config.action_mailer.perform_caching = false
 
