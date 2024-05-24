3d2a9e70a2c66328f299310984da1c613fac583c
Erick Contreras <er.contreras@hotmail.com>
2024-05-24 12:59:16 -0600

    Add Factory bot and faker for testing

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  before do
+    user = build_list(:user, 10)   
+  end
+
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it "shows all users" do
       get users_path
       expect(response).to have_http_status(200)
+      p response.body
     end
   end
 end
