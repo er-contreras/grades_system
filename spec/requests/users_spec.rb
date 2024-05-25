eba9f688cc241c2286ac02f5370df8a6732862b9
Erick Contreras <er.contreras@hotmail.com>
2024-05-25 15:34:20 -0600

    Add spec for user POST

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Users", type: :request do
   let(:json_response) { JSON.parse(response.body) }
 
   before do
-    @user = create_list(:user, 4)
+    @users = create_list(:user, 4)
   end
 
   describe "GET /users" do
@@ -14,6 +14,17 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
       expect(json_response.count).to eq(4)
       expect(json_response).to match_json_schema("users")
+    end
+  end
+
+  describe "POST /users" do
+    it "make succesful request" do
+      user_params = { user: { name: "Joe", email: "joe@email.com" } }
+
+      post users_path, params: user_params
+      
+      expect(response.status).to eq(201)
+      expect(json_response).to match_json_schema("user")
     end
   end
 end
