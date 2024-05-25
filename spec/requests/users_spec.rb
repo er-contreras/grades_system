64e21a55ff9b93946a7b1ebfbcd8ba60ce9b0c56
Erick Contreras <er.contreras@hotmail.com>
2024-05-25 12:40:05 -0600

    Add user get spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  let(:json_response) { JSON.parse(response.body) }
+
   before do
-    user = build_list(:user, 10)   
+    @user = create_list(:user, 4)
   end
 
   describe "GET /users" do
     it "shows all users" do
       get users_path
+
       expect(response).to have_http_status(200)
-      p response.body
+      expect(json_response.count).to eq(4)
+      expect(json_response).to match_json_schema("users")
     end
   end
 end
