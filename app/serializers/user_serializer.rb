d520d8feb403f87fc2cbe08344a8d399a9bcfd95
ercntreras <er.contreras@hotmail.com>
2024-07-04 12:25:13 -0600

    Update user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :role, :created_at, :updated_at
+  attributes :id,
+            :name,
+            :email,
+            :role,
+            :created_at,
+            :updated_at,
+            :some_message
+
+
+  has_many :courses
+
+  def some_message
+    "Hi my name is #{self.object.name}, I am a #{self.object.role} nice to meet you"
+  end
 end
